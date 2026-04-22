f8cc1af07824b35ec3513f6524be17641255e635
irinayar <45104085+irinayar@users.noreply.github.com>
2026-04-22 14:22:02 -0700

    Add files via upload

---
+++ Controls/Javascripts/OUR.js
@@ -0,0 +1,39 @@
+﻿function showSpinner() {
+
+    setTimeout(function () { document.getElementById("spinner").style.display = ""; document.images["imgSpinner"].src = "Controls/Images/WaitImage2.gif"; }, 200);
+}
+    
+function ClearTextbox(ctlId) {
+    //var txt = document.getElementById(ctlId);
+    //if (txt != null)
+    //  txt.value = '';
+    SetText(ctlId, '');
+}
+function ClearNewPswd()
+{
+    ClearTextbox("txtNew");
+    ClearTextbox("txtRepeat");
+}
+function ClearCurrentPswd()
+{
+    ClearTextbox("txtCurrent")
+}
+function SetText(ctlId,text)
+{
+    var txt = document.getElementById(ctlId);
+    if (txt != null)
+        txt.value = text;
+}
+
+function clickFileUpload() {
+    var fileUpload = document.getElementById("FileRDL");
+    if (fileUpload != null) fileUpload.click();
+}
+function getAttachedFile() {
+    var fileUpload = document.getElementById("FileRDL");
+    if (fileUpload != null) {
+        var lblAttached = document.getElementById("lblFileChosen");
+        lblAttached.innerText = fileUpload.files[0].name;
+        
+    }
+}
